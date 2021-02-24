1e1fb54dbf9012f0eb30763676a255d3da8f1b56
patrickandrews-m <patrickandrews.m@ltts.com>
2021-02-24 13:35:48 +0530

    First Version for DemoKart

---
+++ DemoKartBL/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoKartBL
+{
+    public class Cart
+    {
+        //Delete this method before submission
+        //Method was created to generate test project
+        public void SampleMethod()
+        {
+
+        }
+    }
+}
---
+++ DemoKartBLTests/CartTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoKartBL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoKartBL.Tests
+{
+    [TestClass()]
+    public class CartTests
+    {
+        [TestMethod()]
+        public void SampleMethodTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
