e31c9829349e12af74ff358a3f8d08decbcc7d64
99003546 <78855186+99003546@users.noreply.github.com>
2021-02-25 10:49:52 +0530

    Update Product.cs

--- DemoKartBL/Product.cs
+++ DemoKartBL/Product.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace DemoKartBL
 {
+{
     class Product
     {
+        public int pid { get; set; }
+        public String pname { get; set; }
+        public int quantity { get; set; }
+        public double price { get; set; }
+        public Product(int pid,String pname,int quantity,double price)
+        {
+            this.pid = pid;
+            this.pname = pname;
+            this.quantity = quantity;
+            this.price = price;
+        }
+        
+
+
     }
+
 }
+
