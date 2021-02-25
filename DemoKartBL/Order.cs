465fc76a836c55aa77f5ddcdbb5330f48444846c
99003546 <78855186+99003546@users.noreply.github.com>
2021-02-25 10:50:43 +0530

    Update Order.cs

--- DemoKartBL/Order.cs
+++ DemoKartBL/Order.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,21 @@ namespace DemoKartBL
 {
     class Order
     {
+        public String Address;
+        public int pincode;
+        public String name;
+        public int pno;
+
+        public   String Orderlist(String Addr,int pcode, String Name, int Pno)
+        {
+            Address = Addr;
+            pincode = pcode;
+
+            name = Name;
+            pno = Pno;
+            return Address + "\n" + pincode + "\n" + name + "\n" + pno;
+        }
+
     }
 }
+
