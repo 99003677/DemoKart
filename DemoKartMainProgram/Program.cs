76d81078f701c07ef84edec899a2ce89e4fdc414
99003546 <78855186+99003546@users.noreply.github.com>
2021-02-25 12:09:06 +0530

    Update Program.cs

--- DemoKartMainProgram/Program.cs
+++ DemoKartMainProgram/Program.cs
@@ -1,15 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DemoKartMainProgram
+namespace Productcart
 {
     class Program
     {
+      
         static void Main(string[] args)
         {
+            int i;
+            Product[] p = new Product[100];
+            Console.WriteLine("enter username");
+            String username = Console.ReadLine();
+            Console.WriteLine("enter pasword");
+            String password = Console.ReadLine();
+            User u = new User();
+            int res = u.Authent(username, password);
+           
+
         }
     }
 }
+
