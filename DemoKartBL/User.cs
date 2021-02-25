e42790a01c336016e0b30f0e655b3199a523dc98
99003546 <78855186+99003546@users.noreply.github.com>
2021-02-25 10:48:43 +0530

    Update User.cs

--- DemoKartBL/User.cs
+++ DemoKartBL/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,38 @@ namespace DemoKartBL
 {
     class User
     {
+        public String username;
+        public String password;
+        public static List<User> ul = new List<User>();
+        public User()
+        {
+
+            ul.Add(new User("sri", "sri1234"));
+            ul.Add(new User("lekha", "lekha1234"));
+            ul.Add(new User("parimi", "parimi1234"));
+            ul.Add(new User("yamini", "yamini1234"));
+
+        }
+        public User(String name, String pswd)
+        {
+            this.username = name;
+            this.password = pswd;
+        }
+        public int Authent(String username, String password)
+        {
+            if (ul.Contains(new User(username, password)))
+            {
+                return 1;
+
+            }
+            else
+            {
+                ul.Add(new User(username, password));
+                return 0;
+            }
+        }
+
+
     }
 }
+
